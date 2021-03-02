534d5f5c8b9500180bdcc789b0e91c0610570ca7
seattlechem <seattlechem@yahoo.com>
2021-03-02 06:36:24 -0800

    initial commit

---
+++ easy/Rotate2DMatrix.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    public int[][] rotate(int[][] matrix) {
+        int size = matrix.length -1 ;
+
+        for (int layer = 0; layer < matrix.length/2; layer++) {
+            for (int i = layer; i < size - layer; i++) {
+
+            }
+        }
+    }
+}
